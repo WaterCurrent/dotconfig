8f7391dc1cf45bfb0c4b8e0df1e5775e5e37333d
mox <mox@jnkpc02.local>
2020-10-23 23:43:06 +0900

    clean clean

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+cd ~/
+ln -s .zprofile .dotconfig/.zprofile 
+ln -s .zprofile_Darwin .dotconfig/.zprofile_Darwin
+ln -s .zprofile_Linux .dotconfig/.zprofile_Linux
+ln -s .zshrc .dotconfig/.zshrc 
+ln -s .zshrc_Darwin .dotconfig/.zshrc_Darwin
+ln -s .zshrc_Linux .dotconfig/.zshrc_Linux
+ln -s .tmux.conf .dotconfig/.tmux.conf
+ln -s .vimrc .dotconfig/.vimrc
